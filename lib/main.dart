d7484ccc2debad2c9c3b3cb649557af232cf64ed
Nasir Ahmad <nahmad@resqsoft.com>
2022-03-14 06:32:25 -0400

    step -2

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -15,6 +16,10 @@ void main() {
       primarySwatch: Colors.blue,
     ),
     home: const HomePage(),
+    routes: {
+      '/login/':(context) => const LoginView(),
+      '/register/':(context) => const RegisterView(),
+    },
   ));
 }
 
@@ -23,30 +28,26 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: FutureBuilder(
+    return  FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = (FirebaseAuth.instance.currentUser);
+              // final user = (FirebaseAuth.instance.currentUser);
 
-              if (user?.emailVerified ?? false) {
-                print("email verified");
-              } else {
-                print('Please verify email');
-              }
-              return const Text('Done');
+              // if (user?.emailVerified ?? false) {
+              //   return const Text("Done");
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
             default:
-              return Text('loading...');
+              return const CircularProgressIndicator();
           }
         },
-      ),
-    );
+      );
+    
   }
 }
